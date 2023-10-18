4cee354a32ce386f627e20c5ea5cf166b0995de2
Takahiro Egawa <2111140085a@ed.fuk.kindai.ac.jp>
2023-10-18 07:39:24 +0000

    CS1の制作

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,13 +1,17 @@
 class TopController < ApplicationController
   def main
-    render "main"
+    if session[:login_uid] != nil
+      render "main"
+    else
+      render "login"
+    end
   end
 
   def login
-    render "login"
-  end
-
-  def login
-    
+    logger.debug params[:uid]
+    if params[:uid] == 'kindai' and params[:pass] == 'sanriko'
+      session[:login_uid] = params[:uid]
+      redirect_to top_main_path
+    end
   end
 end
