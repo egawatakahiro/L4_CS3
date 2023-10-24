6ff14558a52d18d79e9407115b6516f24f0e5302
Takahiro Egawa <2111140085a@ed.fuk.kindai.ac.jp>
2023-10-24 07:24:27 +0000

    make

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class TopController < ApplicationController
   def main
     if session[:login_uid] != nil
@@ -8,12 +10,12 @@ class TopController < ApplicationController
   end
 
   def login
-    logger.debug params[:uid]
-    if User.find_by(uid: params[:uid]) and BCrypt::Password.new(user.pass) == params[:pass]
+    user = User.find_by(uid: params[:uid])
+    
+    if user && BCrypt::Password.new(user.pass) == params[:pass]
       session[:login_uid] = params[:uid]
       redirect_to top_main_path
     else
-      logger.debug("***")
       render "error"
     end
   end
@@ -22,4 +24,36 @@ class TopController < ApplicationController
     session.delete(:login_uid)
     redirect_to top_main_path
   end
+  
+  def new
+    render "make"
+  end
+  
+  def make
+    make_pass = BCrypt::Password.create(params[:pass])
+    user = User.new(uid: params[:uid], pass: make_pass)
+    user.save
+    session[:login_uid] = params[:uid]
+    redirect_to top_main_path
+  end
 end
+    
+    
+    
+    #logger.debug params[:pass]
+    #logger.debug params[:uid]
+    
+    #if User.find_by(uid: params[:uid]) and BCrypt::Password.new(user.pass) == params[:pass]
+      #session[:login_uid] = params[:uid]
+      #redirect_to top_main_path
+    #else
+      #logger.debug("***")
+      #render "error"
+    #end
+  #end
+  
+  #def logout
+    #session.delete(:login_uid)
+    #redirect_to top_main_path
+  #end
+#end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   post 'top/login'
   root 'top#main'
   get 'top/logout'
+  get 'top/new'
+  post 'top/make'
   
 end
